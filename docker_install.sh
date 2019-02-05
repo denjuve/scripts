00ee2c22cf0f3a7c08e68dc3a838a655ba7dc988
Denis Kucherenko <den.ku4er@gmail.com>
2019-02-05 14:45:47 +0200

    centos test

--- docker_install.sh
+++ docker_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+if [  -n "$(uname -rv | grep -i ubuntu)" ]; then
 sudo apt-get remove docker docker-engine docker.io -y
-
 sudo apt-get update
-
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -17,5 +16,25 @@ sudo add-apt-repository \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-get update
-sudo apt install -y docker-ce=18.06.1~ce~3-0~ubuntu
-#sudo apt-get install docker-ce -y
+#sudo apt install -y docker-ce=18.06.1~ce~3-0~ubuntu
+sudo apt-get install docker-ce -y
+
+else
+sudo yum remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
