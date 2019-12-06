873070ecdf1ffcfd2d572b99bbf5cbe95579ec3c
Denis Kucherenko <den.ku4er@gmail.com>
2019-12-06 10:16:24 +0200

    nets specify quantity+, net default+, ips update(single/multi)+, def-net ip update+

--- kub.sh
+++ kub.sh
@@ -3,56 +3,62 @@ exec 1>log.log 2>&1
 user=ubuntu
 nt=3            #nodes total:
 ver='=1.15.3-00'
+
 node1='node01'
-node1_ip='192.168.122.4'
+ip_node1='10.77.101.10'
 node2='node02'
-node2_ip='192.168.122.14'
+ip_node2='10.77.101.11'
 node3='node03'
-node3_ip='192.168.122.57'
+ip_node3='10.77.101.12'
 
-#nodes IP
-sudo bash -c "cat << EOF >> /etc/hosts
-$node1_ip $node1
-$node2_ip $node2
-$node3_ip $node3
-EOF"
-
-########################
-cat <<EOF > kube_node_install.sh
-#!/bin/bash
-sudo swapoff -a
-sudo modprobe br_netfilter
-sudo sysctl net.bridge.bridge-nf-call-arptables=1
-sudo sysctl net.bridge.bridge-nf-call-ip6tables=1
-sudo sysctl net.bridge.bridge-nf-call-iptables=1
-git clone https://github.com/denjuve/scripts.git 
-if sudo docker version; then echo 'Docker installed'
-else rm -rf /tmp/scripts-my; git clone https://github.com/denjuve/scripts.git /tmp/scripts-my
-bash /tmp/scripts-my/docker_install.sh kube; fi
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet${ver} kubectl${ver} kubeadm${ver}
-EOF
-
-bash kube_node_install.sh
-for ((i = 1; i <= nt; i++)); do
-ssh -i key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${user}@node0${i} 'sudo bash -s' < kube_node_install.sh
-done
-########################
-
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 | tee token.log
-sudo cat token.log | grep -A 2 "kubeadm join " > token.get
-
-for ((i = 2; i <= nt; i++)); do
-ssh -i key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${user}@node0${i} 'sudo bash -s' < kube_node_install.sh
+for ((node_count = 1; node_count <= nt; node_count++)); do
+bash cloud-init-make.sh2 -n $node${node_count} -r 15000 -c 2 -i $ip_node${node_count}
 done
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
-
-sleep 25 && sudo kubectl get pods --all-namespaces
-sleep 25 && sudo kubectl get nodes
+##nodes IP
+#sudo bash -c "cat << EOF >> /etc/hosts
+##for ((node_count = 1; node_count <= nt; node_count++)); do
+#$ip_node1 $node1
+#$ip_node2 $node2
+#$ip_node3 $node3
+#EOF"
+#
+#########################
+#cat <<EOF > kube_node_install.sh
+##!/bin/bash
+#sudo swapoff -a
+#sudo modprobe br_netfilter
+#sudo sysctl net.bridge.bridge-nf-call-arptables=1
+#sudo sysctl net.bridge.bridge-nf-call-ip6tables=1
+#sudo sysctl net.bridge.bridge-nf-call-iptables=1
+#git clone https://github.com/denjuve/scripts.git 
+#if sudo docker version; then echo 'Docker installed'
+#else rm -rf /tmp/scripts-my; git clone https://github.com/denjuve/scripts.git /tmp/scripts-my
+#bash /tmp/scripts-my/docker_install.sh kube; fi
+#sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+#sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+#sudo apt-get update
+#sudo apt-get install -y kubelet${ver} kubectl${ver} kubeadm${ver}
+#EOF
+#
+#bash kube_node_install.sh
+#for ((i = 1; i <= nt; i++)); do
+#ssh -i key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${user}@node0${i} 'sudo bash -s' < kube_node_install.sh
+#done
+#########################
+#
+#sudo kubeadm init --pod-network-cidr=10.244.0.0/16 | tee token.log
+#sudo cat token.log | grep -A 2 "kubeadm join " > token.get
+#
+#for ((i = 2; i <= nt; i++)); do
+#ssh -i key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${user}@node0${i} 'sudo bash -s' < kube_node_install.sh
+#done
+#
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
+#
+#sleep 25 && sudo kubectl get pods --all-namespaces
+#sleep 25 && sudo kubectl get nodes
--- libs.sh
+++ libs.sh
@@ -5,26 +5,25 @@ cat << EOF
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 $(echo "sudo $(basename "$0") -n [node_name] -r [ram(MB)] -c [cpu count] -u [ubuntu | centos] (default - ubuntu) | -h [this hint]")
 $(echo "OPTIONS:
-  -n  created node hostname
-  -r  RAM 
+  -n  VM hostname
+  -r  RAM (MB) 
   -c  cpu count
-  -u  username and OS * Optional default option - ubuntu"
-)
+  -u  username and OS * Optional default option - ubuntu
+  -i  ip address for ctreated net (only in single net case) default value is 10.77.101.100
+  -N  attached network options: default is for default libvirt network, number 2..18 is for attached networks quantity
+")
 
 Example:
-$(echo "sudo $(basename "$0") -n node1 -r 5000 -c 3") 
+$(echo "sudo $(basename "$0") -n node1 -r 5000 -c 3 -u ubuntu -i 10.77.101.250") 
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 EOF
 }
 
-function update_public_network {
-     # set static ip address for node    \/ change this
-cmac=$(virsh domiflist ${node} | awk '/virbr0/ {print $5; exit}')
-echo $cmac; echo $IP_ADDR
-virsh net-update "${net_name}" add ip-dhcp-host \
-    "<host mac='${cmac}' ip='${IP_ADDR}'/>" --live --config
+function get_base_image {
+local base_image=$1
+local image_dir=$2
+wget --progress=dot:giga -P "${image_dir}" -N "${base_image}"
 }
-
 
 function prepare_vms {
   local base_image=$1; shift
@@ -34,11 +33,6 @@ function prepare_vms {
   local _o=${base_image/*\/}
   local _tmp
 
-function get_base_image {
-local base_image=$1
-local image_dir=$2
-wget --progress=dot:giga -P "${image_dir}" -N "${base_image}"
-}
       get_base_image "${base_image}" "${image_dir}"
     ./create-config-drive.sh -k ~/.ssh/id_rsa.pub \
        -u "${user_data}" -h "${node}" "${image_dir}/${node}.iso"
@@ -46,12 +40,49 @@ wget --progress=dot:giga -P "${image_dir}" -N "${base_image}"
     qemu-img resize "${image_dir}/${node}.qcow2" 100G
 }
 
+function create_networks {
+  if [[ $NETWORK == "default" ]]; then
+    echo "Default network specified, no net cration needed"
+  else
+  cn_net=("$@")
+  for net in "${cn_net[@]}"; do
+cat <<EOF > ${net}.xml
+<network>
+  <name>${net}</name>
+  <uuid>$(uuidgen)</uuid>
+  <forward mode='nat'>
+    <nat>
+      <port start='1024' end='65535'/>
+    </nat>
+  </forward>
+  <bridge name='${net}' stp='on' delay='0'/>
+  <ip address='10.${net}.101.1' netmask='255.255.255.0'>
+    <dhcp>
+      <range start='10.${net}.101.2' end='10.${net}.101.254'/>
+    </dhcp>
+  </ip>
+</network>
+EOF
+    virsh net-define ${net}.xml
+    virsh net-autostart ${net}
+    virsh net-start ${net}
+    rm -f ${net}.xml
+  done
+fi
+}
+
 function create_vms {
   local image_dir=$1; shift
-
-net_args="--network bridge=${net_bridge},model=virtio"
-#--network bridge=ub16,model=virtio"
-#--network bridge=virbr0,model=virtio --network bridge=virbr0,model=virtio --network bridge=mngbr0,model=virtio"
+  if [[ $NETWORK == "default" ]]; then
+    net_args="--network bridge=virbr0,model=virtio"
+  else
+    NET_ARGS=()
+    for net_a in ${cn_net[@]}; do
+      net_argument=" --network bridge=${net_a},model=virtio"
+      NET_ARGS+=($net_argument)
+    done
+    net_args="${NET_ARGS[*]}"        ##" --network bridge=virbr0,model=virtio --network bridge=mngbr0,model=virtio"
+  fi
 
     virt-install --name "${node}" \
     --ram "${ram}" --vcpus "${cpu}" \
@@ -62,4 +93,46 @@ net_args="--network bridge=${net_bridge},model=virtio"
     --disk path="${image_dir}/${node}.iso",device=cdrom \
     --noautoconsole \
     ${virt_extra_args}
+}
+
+function update_public_network {
+
+  ##for net in ${ARRAY[@]}; do
+  # !!!Default network options
+  if [[ $NETWORK == "default" ]]; then
+    cmac=$(virsh domiflist ${node} |grep default | awk '{print $5}')
+    echo "mac: $cmac"; echo "ip: $ip_addr"
+  elif [[ $NETWORK == "default" ]] && [ -n "${ip_addr}" ]; then
+    cmac=$(virsh domiflist ${node} |grep default | awk '{print $5}')
+    virsh net-update ${net} add ip-dhcp-host \
+        "<host mac='${cmac}' ip='${ip_addr}'/>" --live --config
+  
+  # !!!Custom IP in a single arbitrary network***
+  elif [ -n "${ip_addr}" ] && [ ${#cn_net[@]} == 1 ]; then
+  ARRAY=()
+  for i in ${cn_net[@]}; do
+    echo "net${i} is: $i"; ARRAY+=(10.${i}.101.100)
+  done
+  echo "nets are: ${cn_net[@]}";echo "IP(s) in nets are ${ARRAY[@]}"
+    local ip_addr=$1
+    cmac=$(virsh domiflist ${node} |grep "${cn_net[0]}" | awk '{print $5}')   #cmac=$(virsh domiflist ${node} | awk 'FNR == 3 {print $5}')
+    echo "mac: $cmac"; echo "ip: $ip_addr"
+    virsh net-update ${net} add ip-dhcp-host \
+        "<host mac='${cmac}' ip='${ip_addr}'/>" --live --config
+  else
+  ##done
+  # !!!Arbitrary quantity of networks (up to 18) predefined (hardcoded) .100 IPs
+  ARRAY=()
+  for i in ${cn_net[@]}; do
+    echo "net${i} is: $i"; ARRAY+=(10.${i}.101.100)
+  done
+  echo "nets are: ${cn_net[@]}";echo "IP(s) in nets are ${ARRAY[@]}"
+    for ((ips = 0; ips < ${#cn_net[@]}; ips++)); do
+      cmac=$(virsh domiflist ${node} | grep "${cn_net[ips]}" | awk '{print $5}')
+      printf "%s is in %s\n" "${ARRAY[ips]}" "${cn_net[ips]}"
+      echo "mac: $cmac"; echo "ip: ${ARRAY[ips]}"
+      virsh net-update ${cn_net[ips]} add ip-dhcp-host \
+          "<host mac='${cmac}' ip='${ARRAY[ips]}'/>" --live --config
+    done
+  fi
 }
