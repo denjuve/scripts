9322a36874af60c09a79177086190b13611cc5aa
root <den_juve@list.ru>
2020-04-08 14:46:20 +0000

    put repo key Before repo add

--- docker_install.sh
+++ docker_install.sh
@@ -9,12 +9,13 @@ sudo apt-get install -y \
     curl \
     software-properties-common
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-get update
 #18.06.1~ce~3-0~ubuntu
