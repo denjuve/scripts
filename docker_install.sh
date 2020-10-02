c5b4e53d003198cf0ed1c9908e7ab04920fcef5b
root <den_juve@list.ru>
2020-10-02 12:04:40 +0000

    add compose installation tu ubuntu

--- docker_install.sh
+++ docker_install.sh
@@ -27,7 +27,8 @@ else
 sudo apt install -y docker-ce=18.06.2~ce~3-0~ubuntu
 #17.03.2~ce-0~ubuntu-xenial
 fi
-
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 else
 sudo yum remove -y docker \
