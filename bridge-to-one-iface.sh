2437cb0408545ea3e5a8d2f35a23590ed88d72e7
root <den_juve@list.ru>
2021-01-22 12:02:08 +0100

    change bridge to one iface

--- bridge-to-one-iface.sh
+++ bridge-to-one-iface.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-iface = eth1
-bridge = br-joint
-gtw_ip = 192.168.122.1
-my_ip = 192.168.122.151
-prefix = 24
-checking_ip = 192.168.122.152
+iface= em1
+bridge= br-joint
+gtw_ip= 10.5.0.1
+my_ip= 10.5.1.18
+prefix= 16
+checking_ip= 10.5.1.14
 
 exec 1>log.log 2>&1
 sleep 20
+ip link set $bridge up
 ip addr flush dev $iface
 brctl addif $bridge $iface
 ip address add $my_ip/$prefix dev $bridge
+ip r del default
+ip r add default via $gtw_ip dev $bridge
 
 if ping $checking_ip -c 10
 then
@@ -19,6 +22,8 @@ else
 ip addr flush dev $bridge
 brctl delif $bridge $iface
 ip address add $my_ip/$prefix dev $iface
+ip r del default
+ip r add default via $gtw_ip dev $iface
 fi
 
 if ping $checking_ip -c 10
